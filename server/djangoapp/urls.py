e75d674cd75c9fa53ec40746d3b802819e2f4958
sebastianclavel <sebastianclavel94@gmail.com>
2023-09-04 14:11:43 -0400

    se agregaron paginas

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,14 +16,18 @@ urlpatterns = [
     path(route='contact/', view=views.contact, name='contact'),
 
     # path for registration
+    path(route='registration/', view=views.registration_request, name='registration'),
 
     # path for login
+    path(route='login/', view=views.login_request, name='login'),
 
     # path for logout
+    path(route='logout/', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
 
     # path for add a review view
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -34,14 +34,42 @@ def contact(request):
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
 # ...
+def login_request(request):
+    context = {}
+    if request.method == "POST":
+        # pull from dictionary
+        username = request.POST['username']
+        password = request.POST['psw']
+        # check auth
+        user = authenticate(username=username, password=password) 
+        if user is not None:
+            # login if valid
+            login(request, user)
+            return render(request, 'djangoapp/index.html', context)
+        else:
+            return render(request, 'djangoapp/index.html', context)
+    else:
+        return render(request, 'djangoapp/index.html', context)
 
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
 # ...
+def logout_request(request):
+    context = {}
+    # get user from session id
+    print("Log out the user `{}`".format(request.user.username))
+    logout(request)
+    # redirect back to the index.html
+    return render(request, 'djangoapp/index.html', context)
 
 # Create a `registration_request` view to handle sign up request
 # def registration_request(request):
 # ...
+def registration_request(request):
+    context = {}
+    # rend if it is a GET req
+    if request.method == 'GET':
+        return render(request, 'djangoapp/registration.html', context)
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
@@ -53,6 +81,12 @@ def get_dealerships(request):
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
 # ...
+def get_dealer_details(request, dealer_id):
+    if request.method == "GET":
+        # Get dealers from the URL
+        context = {}
+        if request.method == "GET":
+            return render(request, 'djangoapp/dealer_details.html', context)
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
